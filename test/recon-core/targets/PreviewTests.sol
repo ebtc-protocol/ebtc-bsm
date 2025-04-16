42b0be3a148237ddd2260875b9db5103a0ef8a01
entreprenerd <alex@entreprenerd.xyz>
2025-04-16 18:40:44 +0200

    fix: equivalence tests

--- test/recon-core/targets/PreviewTests.sol
+++ test/recon-core/targets/PreviewTests.sol
@@ -8,43 +8,101 @@ import {vm} from "@chimera/Hevm.sol";
 
 abstract contract PreviewTests is BaseTargetFunctions, Properties {
     function equivalence_bsm_previewBuyAsset(uint256 _ebtcAmountIn) public stateless {
-        require(escrow.totalBalance() > 1e18, "Min bal"); // Should not matter
+        uint256 amtOut;
+        bool previewRevert;
 
-        uint256 amtOut = bsmTester.previewBuyAsset(_ebtcAmountIn);
+        try bsmTester.previewBuyAsset(_ebtcAmountIn) returns (uint256 amt) {
+            amtOut = amt;
+        } catch {
+            previewRevert = true;
+        }
+        
+        uint256 realOut;
+        bool buyRevert;
 
         vm.prank(_getActor());
-        uint256 realOut = bsmTester.buyAsset(_ebtcAmountIn, _getActor(), 0);
+        try bsmTester.buyAsset(_ebtcAmountIn, _getActor(), 0) returns (uint256 amt) {
+            realOut = amt;
+        } catch {
+            buyRevert = true;
+        }
 
-        eq(realOut, amtOut, "equivalence_bsm_previewBuyAsset");
+        t(buyRevert == previewRevert, "reverts equivalence_bsm_previewBuyAsset");
+        eq(realOut, amtOut, "amt equivalence_bsm_previewBuyAsset");
     }
 
     function equivalence_bsm_previewSellAsset(uint256 _assetAmountIn) public stateless {
-        uint256 amtOut = bsmTester.previewSellAsset(_assetAmountIn);
+        uint256 amtOut;
+        bool previewRevert;
+
+        try bsmTester.previewSellAsset(_assetAmountIn) returns (uint256 amt) {
+            amtOut = amt;
+        } catch {
+            previewRevert = true;
+        }
+
+        
+        uint256 realOut;
+        bool buyRevert;
 
         vm.prank(_getActor());
-        uint256 realOut = bsmTester.sellAsset(_assetAmountIn, _getActor(), 0);
+        try bsmTester.sellAsset(_assetAmountIn, _getActor(), 0) returns (uint256 amt) {
+            realOut = amt;
+        } catch {
+            buyRevert = true;
+        }
 
-        eq(realOut, amtOut, "equivalence_bsm_previewSellAsset");
+        t(buyRevert == previewRevert, "reverts equivalence_bsm_previewSellAsset");
+        eq(realOut, amtOut, "amt equivalence_bsm_previewSellAsset");
     }
 
     // Feeless comparison
     function equivalence_bsm_previewBuyAssetNoFee(uint256 _ebtcAmountIn) public stateless {
-        require(escrow.totalBalance() > 1e18, "Min bal"); // Should not matter
+        uint256 amtOut;
+        bool previewRevert;
 
-        uint256 amtOut = bsmTester.previewBuyAssetNoFee(_ebtcAmountIn);
+        try bsmTester.previewBuyAssetNoFee(_ebtcAmountIn) returns (uint256 amt) {
+            amtOut = amt;
+        } catch {
+            previewRevert = true;
+        }
+
+        
+        uint256 realOut;
+        bool buyRevert;
 
         vm.prank(_getActor());
-        uint256 realOut = bsmTester.buyAssetNoFee(_ebtcAmountIn, _getActor(), 0);
+        try bsmTester.buyAssetNoFee(_ebtcAmountIn, _getActor(), 0) returns (uint256 amt) {
+            realOut = amt;
+        } catch {
+            buyRevert = true;
+        }
 
-        eq(realOut, amtOut, "equivalence_bsm_previewBuyAsset");
+        t(buyRevert == previewRevert, "reverts equivalence_bsm_previewBuyAssetNoFee");
+        eq(realOut, amtOut, "amt equivalence_bsm_previewBuyAssetNoFee");
     }
 
     function equivalence_bsm_previewSellAssetNoFee(uint256 _assetAmountIn) public stateless {
-        uint256 amtOut = bsmTester.previewSellAssetNoFee(_assetAmountIn);
+        uint256 amtOut;
+        bool previewRevert;
 
+        try bsmTester.previewSellAssetNoFee(_assetAmountIn) returns (uint256 amt) {
+            amtOut = amt;
+        } catch {
+            previewRevert = true;
+        }
+
+        
+        uint256 realOut;
+        bool buyRevert;
         vm.prank(_getActor());
-        uint256 realOut = bsmTester.sellAssetNoFee(_assetAmountIn, _getActor(), 0);
+        try bsmTester.sellAssetNoFee(_assetAmountIn, _getActor(), 0) returns (uint256 amt) {
+            realOut = amt;
+        } catch {
+            buyRevert = true;
+        }
 
-        eq(realOut, amtOut, "equivalence_bsm_previewSellAsset");
+        t(buyRevert == previewRevert, "reverts equivalence_bsm_previewSellAssetNoFee");
+        eq(realOut, amtOut, "amt equivalence_bsm_previewSellAssetNoFee");
     }
 }
