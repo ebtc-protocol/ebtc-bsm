e9466f096f1ea53c36b8ea7f25e4ac3656370c3d
JulissaDantes <julissadcj@gmail.com>
2025-05-01 15:06:27 -0400

    adjusting to use set roles and addresses

--- test/fork-tests/BSMForkTests.t.sol
+++ test/fork-tests/BSMForkTests.t.sol
@@ -25,44 +25,12 @@ contract BSMForkTests is Test {
     BaseEscrow public baseEscrow = BaseEscrow(0x686FdecC0572e30768331D4e1a44E5077B2f6083);
     EbtcBSM public ebtcBSM = EbtcBSM(0x828787A14fd4470Ef925Eefa8a56C88D85D4a06A);
     address cbBtcPool = 0xe8f7c89C5eFa061e340f2d2F206EC78FD8f7e124;
-    address testAuthorizedAccount = address(0x1);
-    address owner;
+    address bsmAdmin = 0xaDDeE229Bd103bb5B10C3CdB595A01c425dd3264;
     uint256 initBlock = 22384650;// Block where BSM and peripheral contracts were already deployed and governance roles set
-    //TODO modify existing tests to use correct roles
-    modifier prankDefaultGovernance() {
-        vm.prank(owner);
-        _;
-    }
 
     function setUp() public {
         uint256 forkId = vm.createFork(vm.envString("RPC_URL"), initBlock);
         vm.selectFork(forkId);
-        owner = authority.owner();
-
-        // give eBTC minter and burner roles
-        /*setUserRole(address(ebtcBSM), 1, true); already at current block
-        setUserRole(address(ebtcBSM), 2, true);*/
-
-        setRoleCapability(
-            15,
-            address(ebtcBSM),
-            ebtcBSM.setFeeToBuy.selector,
-            true
-        );
-        setRoleCapability(
-            15,
-            address(ebtcBSM),
-            ebtcBSM.setFeeToSell.selector,
-            true
-        );
-        setRoleCapability(
-            15,
-            address(rateLimitingConstraint),
-            rateLimitingConstraint.setMintingConfig.selector,
-            true
-        );
-
-        setUserRole(testAuthorizedAccount, 15, true);
     }
 
     // Deployment tests
@@ -74,6 +42,8 @@ contract BSMForkTests is Test {
         assertEq(address(ebtcBSM.buyAssetConstraint()), address(dummyConstraint));
         assertEq(address(ebtcBSM.ASSET_TOKEN()), address(cbBtc));
         assertEq(address(ebtcBSM.EBTC_TOKEN()), address(ebtc));
+        assertTrue(contains(authority.getRolesForUser(address(ebtcBSM)), 1));
+        assertTrue(contains(authority.getRolesForUser(address(ebtcBSM)), 2));
 
         // Check initialization
         vm.expectRevert(abi.encodeWithSelector(Initializable.InvalidInitialization.selector));
@@ -85,9 +55,6 @@ contract BSMForkTests is Test {
         assertEq(config.relativeCapBPS, 0);
         assertEq(config.absoluteCap, 0);
         assertEq(config.useAbsoluteCap, false);
-
-        // ebtcBsm user roles
-        // 1 and 2 must have TODO
     }
 
     // AUTH tests
@@ -148,57 +115,57 @@ contract BSMForkTests is Test {
 
         // Basic sell
         vm.prank(cbBtcPool);// Fund account
-        cbBtc.transfer(testAuthorizedAccount, amount);
+        cbBtc.transfer(bsmAdmin, amount);
         
-        vm.prank(testAuthorizedAccount);
+        vm.prank(bsmAdmin);
         cbBtc.approve(address(ebtcBSM), amount);
 
-        assertEq(cbBtc.balanceOf(testAuthorizedAccount), amount);
-        assertEq(ebtc.balanceOf(testAuthorizedAccount), 0);
+        assertEq(cbBtc.balanceOf(bsmAdmin), amount);
+        assertEq(ebtc.balanceOf(bsmAdmin), 0);
 
-        vm.prank(testAuthorizedAccount);
+        vm.prank(bsmAdmin);
         rateLimitingConstraint.setMintingConfig(address(ebtcBSM), RateLimitingConstraint.MintingConfig(relativeCapBPS, 0, false));
         
         vm.expectEmit();
         emit IEbtcBSM.AssetSold(amount, ebtcAmount, 0);
 
-        vm.prank(testAuthorizedAccount);
-        uint256 sellResult = ebtcBSM.sellAsset(amount, testAuthorizedAccount, 0);
+        vm.prank(bsmAdmin);
+        uint256 sellResult = ebtcBSM.sellAsset(amount, bsmAdmin, 0);
         assertEq(sellResult, ebtcAmount);
 
         // These 2 checks also check that ebtcBSM.totalMinted() == baseEscrow.totalAssetsDeposited()
         assertEq(ebtcBSM.totalMinted(), ebtcAmount);
         assertEq(baseEscrow.totalAssetsDeposited(), amount);
 
-        assertEq(cbBtc.balanceOf(testAuthorizedAccount), 0);
-        assertEq(ebtc.balanceOf(testAuthorizedAccount), ebtcAmount);
+        assertEq(cbBtc.balanceOf(bsmAdmin), 0);
+        assertEq(ebtc.balanceOf(bsmAdmin), ebtcAmount);
         assertEq(cbBtc.balanceOf(address(ebtcBSM.escrow())), amount);
 
         // Basic buy
         assertEq(ebtcBSM.previewBuyAsset(ebtcAmount), amount);
 
         vm.recordLogs();
-        vm.prank(testAuthorizedAccount);
+        vm.prank(bsmAdmin);
 
-        assertEq(ebtcBSM.buyAsset(ebtcAmount, testAuthorizedAccount, 0), amount);
+        assertEq(ebtcBSM.buyAsset(ebtcAmount, bsmAdmin, 0), amount);
         Vm.Log[] memory entries = vm.getRecordedLogs();
 
         assertEq(entries[0].topics[0], keccak256("Transfer(address,address,uint256)"));
         assertEq(entries[1].topics[0], keccak256("Transfer(address,address,uint256)"));
         assertEq(entries[2].topics[0], keccak256("AssetBought(uint256,uint256,uint256)"));
 
-        assertEq(cbBtc.balanceOf(testAuthorizedAccount), amount);
-        assertEq(ebtc.balanceOf(testAuthorizedAccount), 0);
+        assertEq(cbBtc.balanceOf(bsmAdmin), amount);
+        assertEq(ebtc.balanceOf(bsmAdmin), 0);
     }
     // TODO extract the setup and make a baseTest for fork tests
+    // TODO create helper to avoid repeating code so much
     function testAdminRole() public {
-        address user = 0xaDDeE229Bd103bb5B10C3CdB595A01c425dd3264;
+        address user = bsmAdmin;
         uint8 roleId = 15;
 
         assertTrue(contains(authority.getRolesForUser(user), roleId));
         assertEq(authority.getRoleName(roleId), "BSM: Admin");
-        
-        //TODO check correct capabilities by role and correct targets
+
         authority.doesRoleHaveCapability(roleId, address(ebtcBSM), 0x8b6a101a);
         authority.doesRoleHaveCapability(roleId, address(ebtcBSM), 0x037ba2ab);
         authority.doesRoleHaveCapability(roleId, address(ebtcBSM), 0xe19e50d4);
@@ -279,22 +246,6 @@ contract BSMForkTests is Test {
     }
 
     // Helpers
-    function setUserRole(address _user, uint8 _role, bool _enabled) internal prankDefaultGovernance {
-        authority.setUserRole(_user, _role, _enabled);
-    }
-    
-    function setRoleCapability(uint8 _role,
-            address _target,
-            bytes4 _functionSig,
-            bool _enabled) internal prankDefaultGovernance {
-        authority.setRoleCapability(
-            _role,
-            _target,
-            _functionSig,
-            _enabled
-        );
-    }
-
     function contains(uint8[] memory arr, uint8 value) internal pure returns (bool) {
         for (uint i = 0; i < arr.length; i++) {
             if (arr[i] == value) {
